5cad357f1d6534f359a58fd54e4dc09e5a1cf5e6
Joel Kuzmarski <leoj3n@gmail.com>
2024-03-09 04:14:04 -0600

    init

---
+++ src/lib/MyButtonTS.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	import type { HTMLButtonAttributes } from 'svelte/elements';
+
+	interface $$Props extends HTMLButtonAttributes {
+		/** What should my name be? */
+		myName: string;
+		/** My custom prop! */
+		myCustomProp: boolean;
+	}
+
+	export let myName: $$Props['myName'] = 'Foo';
+	export let myCustomProp: $$Props['myCustomProp'];
+</script>
+
+<button {...$$restProps}>
+	{myName}
+	{#if myCustomProp}isTrue{/if}
+	<slot />
+</button>
+
+<!--
+@component
+Here's some documentation for this component...
+
+## Usage
+- Simple usage:
+```tsx
+<MyButton>Foo</MyButton>
+```
+- Advanced usage:
+```tsx
+<MyButton myName="Foo" myCustomProp={true}>Bar</MyButton>
+```
+
+## Props
+@prop export let myCustomProp: boolean;
+@prop export let myName: string;
+-->
---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+	import MyButtonTs from '$lib/MyButtonTS.svelte';
+</script>
+
+<h1>My Button TS</h1>
+
+<MyButtonTs myName="TS Btn" myCustomProp={true}></MyButtonTs>
